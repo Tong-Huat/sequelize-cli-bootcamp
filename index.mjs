89dbdadbce870980fefd547ff9ba2b686a21f72c
tong-huat <tonghuat86@gmail.com>
2021-09-10 23:26:01 +0800

    comfy done

--- index.mjs
+++ index.mjs
@@ -1,5 +1,8 @@
 /* eslint-disable max-len */
+import { Sequelize } from 'sequelize';
 import db from './models/index.mjs';
+
+const { Op } = Sequelize;
 
 const [, , action, ...args] = process.argv;
 
@@ -14,6 +17,11 @@ if (action === 'create') {
     .catch((error) => console.log(error));
 }
 
+// node index.mjs add-attrac "Christmas PH" "Art Science Museum" museum
+// attraction id, name, trip_id, category_id
+let tripId;
+let catId;
+
 if (action === 'add-attrac') {
   db.Trip.findOne({
     where: {
@@ -25,18 +33,18 @@ if (action === 'add-attrac') {
         name: process.argv[5],
       },
     }))
-    // .then((returnedTrip, returnedCategory) => db.Attraction.create({
-    //   name: process.argv[4],
-    //   trip_id: returnedTrip.id,
-    //   category_id: returnedCategory.id,
-    // }))
+    .then((returnedTrip, returnedCategory) => db.Attraction.create({
+      name: process.argv[4],
+      trip_id: returnedTrip.id,
+      category_id: returnedCategory.id,
+    }))
     .then((returnedAttraction, returnedCategory) => {
       console.log('Attraction added');
       console.log(returnedAttraction.id, 'returned Attraction ID');
       console.log(returnedAttraction.trip_id, 'returned Attraction trip id');
-      // console.log('Category added');
-      // console.log(returnedCategory.id, 'returned Category ID');
-      // console.log(returnedCategory.trip_id, 'returned Category trip id');
+      console.log('Category added');
+      console.log(returnedCategory.id, 'returned Category ID');
+      console.log(returnedCategory.trip_id, 'returned Category trip id');
     })
     .catch((error) => {
       console.log(error);
@@ -63,4 +71,74 @@ if (action === 'add-category') {
       console.log(category);
     })
     .catch((error) => console.log(error));
+}
+
+if (action === 'category-trip')
+{
+  const tripName = process.argv[3];
+  const catName = process.argv[4];
+  // node index.mjs category-trip "Christmas PH" museum
+  // get all attractions in trip with given category
+
+  db.Trip.findOne({
+    where: {
+      name: tripName,
+    },
+  })
+    .then(({ dataValues }) => {
+      console.log('result from trip', dataValues);
+      tripId = dataValues.id;
+      return db.Category.findOne({
+        where: {
+          name: catName,
+        },
+      });
+    }).then(({ dataValues }) => {
+      console.log('result from category', dataValues);
+      catId = dataValues.id;
+
+      return db.Attraction.findAll({
+        where: {
+          category_id: catId,
+          trip_id: tripId,
+        },
+      });
+    })
+    .then((attractions) => {
+      console.log('Attraction found', attractions.map((attr) => attr.dataValues.name));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// node index.mjs category-attractions <CATEGORY>
+if (action === 'category-attractions') {
+  const catName = process.argv[3];
+  db.Category.findOne({
+    where: {
+      name: catName,
+    },
+  }).then(({ dataValues }) => {
+    catId = dataValues.id;
+    return db.Attraction.findAll({
+      where: {
+        category_id: catId,
+      },
+    });
+  }).then((attractions) => {
+    // get the trips
+    const tripIds = attractions.map((attr) => attr.dataValues.trip_id);
+    console.log('tripids', tripIds);
+    return db.Trip.findAll({
+      where: {
+        id: {
+          [Op.or]: tripIds,
+        },
+      },
+    });
+  }).then((trips) => {
+    console.log(trips.map((trip) => trip.name));
+  })
+    .catch((err) => console.error(err));
 }
--- seeders/20210909060127-seed-data.js
+++ seeders/20210909060127-seed-data.js
@@ -1,24 +1,67 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const tripList = [
+      {
+        name: 'Christmas PH',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'New Year PH',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+    const trips = await queryInterface.bulkInsert(
+      'trips',
+      tripList,
+      { returning: true },
+    );
+
+    const categoryList = [
+      {
+        name: 'museum',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'nature',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
+    const categories = await queryInterface.bulkInsert(
+      'categories',
+      categoryList,
+      { returning: true },
+    );
+
+    const attractionsList = [
+      {
+        name: 'Sentosa',
+        trip_id: 1,
+        created_at: new Date(),
+        updated_at: new Date(),
+        category_id: 1,
+      },
+      {
+        name: 'ECP',
+        trip_id: 2,
+        created_at: new Date(),
+        updated_at: new Date(),
+        category_id: 1,
+      },
+    ];
+
+    const attractions = await queryInterface.bulkInsert(
+      'attractions',
+      attractionsList,
+      { returning: true },
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    await queryInterface.bulkDelete('trips', null, {});
+    await queryInterface.bulkDelete('attractions', null, {});
+  },
 };
